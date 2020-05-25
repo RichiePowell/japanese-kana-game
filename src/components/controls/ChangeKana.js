26f71992a393bed42a4ed476415856eab2fde68e
Rich Powell <rich@wearecore.co.uk>
2020-05-25 22:59:43 +0100

    Remove unnecessary ref

--- src/components/controls/ChangeKana.js
+++ src/components/controls/ChangeKana.js
@@ -1,14 +1,10 @@
 import React from 'react';
 
 const ChangeKana = ({handleKanaChange}) => {
-
-  const selectKana = React.useRef();
-
   return (
     <select
-      onChange={ () => handleKanaChange(selectKana.current.value) }
+      onChange={ (e) => handleKanaChange(e.target.value) }
       className="kana"
-      ref={selectKana}
     >
       <option value="both">Both</option>
       <option value="hiragana">Hiragana</option>
