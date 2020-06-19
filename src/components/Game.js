2ac0df01637e3e1b7a4e38563a65cb26831c5f6d
Rich Powell <rich@wearecore.co.uk>
2020-06-19 01:21:17 +0100

    Remove old unused component

--- src/components/Game.js
+++ src/components/Game.js
@@ -10,7 +10,6 @@ import Score from './Score'
 import Character from './Character'
 import Input from './Input'
 import Controls from './Controls'
-// import Timer from './Timer'
 import { CountdownCircleTimer } from 'react-countdown-circle-timer'
 
 export const Game = () =>
