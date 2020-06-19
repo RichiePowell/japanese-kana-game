37a25734211a3ff74675b1e4ecf8e7e16ba8ee08
Rich Powell <rich@wearecore.co.uk>
2020-06-19 06:10:13 +0100

    Fix forgotten imports

--- src/components/controls/start/Options.js
+++ src/components/controls/start/Options.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import { Consumer } from '../../context'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { ChangeAnswerTimer } from 'ChangeAnswerTimer'
 import { ChangeGameTimer } from 'ChangeGameTimer'
+import { ToggleShowCorrectAnswer } from 'ToggleShowCorrectAnswer'
 
 const Options = () =>
   <Consumer>
