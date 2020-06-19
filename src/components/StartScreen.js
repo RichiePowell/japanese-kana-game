61570257e4da16f71d44296490281d099c6598b8
Rich Powell <rich@wearecore.co.uk>
2020-06-19 06:16:56 +0100

    Code refactor

--- src/components/StartScreen.js
+++ src/components/StartScreen.js
@@ -1,22 +1,15 @@
 import React from 'react'
-import { Consumer } from './context'
 import Header from './Header'
 import ChangeKana from './controls/start/ChangeKana'
 import Options from './controls/start/Options'
 import StartButton from './controls/start/StartButton'
 
 export const StartScreen = () =>
-  <Consumer>
-    { ({ actions }) => {
-      return (
-        <div className="start-screen">
-          <Header />
-          <ChangeKana />
-          <Options />
-          <StartButton />
-        </div>
-      )
-    }}
-  </Consumer>
+  <div className="start-screen">
+    <Header />
+    <ChangeKana />
+    <Options />
+    <StartButton />
+  </div>
 
 export default StartScreen
