beb20a229b3c67e5723b26f1d6a7d202d4ec7425
Rich Powell <rich@wearecore.co.uk>
2020-06-21 10:06:15 +0100

    Remove irrelevant export declaration

--- src/components/StartScreen.js
+++ src/components/StartScreen.js
@@ -4,7 +4,7 @@ import ChangeKana from './controls/start/ChangeKana'
 import Options from './controls/start/Options'
 import StartButton from './controls/start/StartButton'
 
-export const StartScreen = () =>
+const StartScreen = () =>
   <div className="start-screen">
     <Header />
     <ChangeKana />
